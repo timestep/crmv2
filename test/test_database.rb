2b07288963187117d46388f59c8d2edec90f5884
Jin <hojin.yoo@outlook.com>
2013-06-11 15:34:39 -0400

    made class contact

--- test/test_database.rb
+++ test/test_database.rb
@@ -6,33 +6,32 @@ require "../database.rb"
 class DatabaseTest < Test::Unit::TestCase
 
 	def setup
-		@test = Database.new
+		@testdatabase = Database.new
+		@testcontact = Contact.new
 	end
 
 	def test_can_make_new_data_base
-		assert @test
+		assert @testdatabase
 	end
 
-	def test_array_initialize
-		assert @test.database.is_a? Array
-	end
+	# def test_database_initialize
+	# 	assert @testdatabase.database.is_a? Array
+	# end
 
-	def test_empty_array
-		assert_equal [], @test.database
+	def test_database_initialize_creates_empty_array
+		assert_equal [], @testdatabase.database
 	end
 
 	# def test_make_new_contact
 	# 	assert @contact 
 	# end
 
-	def test_check_contact
-		@test.database_status
-		assert @test.database[0].is_a? Hash
+	def test_contact_initialize_creates_empty_hash
+		assert @testcontact.contact.is_a? Hash
 	end
 
 	def test_check_contact_add
-		user = {:FirstName => "Chuckles"}
-		@test.database.add(user)
-		assert @test.database[1].is_a? Hash
+		@testdatabase.add_contact(@testcontact.contact)
+		assert @testdatabase.database[0].is_a? Hash
 	end
 end
